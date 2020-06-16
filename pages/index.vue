9ff7cc0c0f0d0cc30ac4deb3a54878699f9c00bf
Simon Truong <simontruong2@gmail.com>
2020-06-16 22:36:37 +1000

    feat: added tailwindcss

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,26 @@
 <template>
-  <div></div>
+  <section class="container">
+    <div class="grid grid-cols-3 gap-8">
+      <div class="col-span-1 bg-gray-200 px-4 pb-12">
+        <h3 class="text-xl mb-4">My Blog</h3>
+        <ul>
+          <li>
+            <nuxt-link to="/">Home</nuxt-link>
+          </li>
+          <li>
+            <nuxt-link to="/about">About</nuxt-link>
+          </li>
+        </ul>
+      </div>
+
+      <div class="col-span-2">
+        <div class="mt-12">
+          <h1 class="text-5xl pb-2">Some title</h1>
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum</p>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
-<script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
-
-@Component
-export default class YourComponent extends Vue {}
-</script>
-
-<style scoped></style>
+<script></script>
