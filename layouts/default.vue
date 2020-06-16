b32691cd4291e7fa0209b50321b113605725e4de
Simon Truong <simontruong2@gmail.com>
2020-06-16 14:10:27 +1000

    feat: project clean up

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,23 @@
 <template>
-  <nuxt />
+  <v-container fluid>
+    <v-row>
+      <v-col cols="3" />
+
+      <v-col cols="6">
+        <v-card>
+          <v-toolbar dense>
+            <v-toolbar-title>anecdote</v-toolbar-title>
+
+            <v-spacer></v-spacer>
+          </v-toolbar>
+
+          <nuxt />
+        </v-card>
+      </v-col>
+
+      <v-col cols="3" />
+    </v-row>
+  </v-container>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,5 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col cols="3" />
-
-      <v-col cols="6">
-        <v-card>
-          <v-toolbar dense>
-            <v-toolbar-title>anecdote</v-toolbar-title>
-
-            <v-spacer></v-spacer>
-          </v-toolbar>
-        </v-card>
-      </v-col>
-
-      <v-col cols="3" />
-    </v-row>
-  </v-container>
+  <div></div>
 </template>
 
 <script lang="ts">
