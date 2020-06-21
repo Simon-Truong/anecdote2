847d41ceb1dfa9248d2048c0add0b6673fdecbd2
Simon Truong <simontruong2@gmail.com>
2020-06-21 17:38:13 +1000

    refactor: moved radio buttons into google charts component

--- components/google-charts/google-charts.vue
+++ components/google-charts/google-charts.vue
@@ -1,21 +1,65 @@
 <template>
-  <GChart type="GeoChart" :data="data" :settings="settings" :options="options" :events="chartEvents" />
+  <v-container>
+    <v-row class="flex justify-around">
+      <span v-for="continentOption in continentOptions" :key="continentOption.id">
+        <input :id="continentOption.id" v-model="selectedContinent" type="radio" name="region" :value="continentOption.value" />
+        <label :for="continentOption.id">{{ continentOption.label }}</label>
+      </span>
+    </v-row>
+
+    <GChart type="GeoChart" :data="data" :settings="settings" :options="options" :events="chartEvents" />
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 import { GChart } from 'vue-google-charts';
+import ContinentOption from '../../models/continent-option.model';
 import countries from './countries.js';
 
 @Component({ components: { GChart } })
 export default class GoogleCharts extends Vue {
+  selectedContinent: string = 'world';
+
+  continentOptions: ContinentOption[] = [
+    {
+      id: 'world',
+      label: 'World',
+      value: 'world',
+    },
+    {
+      id: 'africa',
+      label: 'Africa',
+      value: '002',
+    },
+    {
+      id: 'europe',
+      label: 'Europe',
+      value: '150',
+    },
+    {
+      id: 'americas',
+      label: 'Americas',
+      value: '019',
+    },
+    {
+      id: 'asia',
+      label: 'Asia',
+      value: '142',
+    },
+    {
+      id: 'oceania',
+      label: 'Oceania',
+      value: '009',
+    },
+  ];
+
   settings: any = {
     packages: ['geochart'],
     mapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
   };
 
   options: { [key: string]: string | number } = {
-    // displayMode: 'text',
     height: 600,
   };
 
---
+++ models/continent-option.model.ts
@@ -0,0 +1,5 @@
+export default interface ContinentOption {
+  id: string;
+  label: string;
+  value: string;
+}
