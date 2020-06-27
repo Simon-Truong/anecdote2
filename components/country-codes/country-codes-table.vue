ac9b022d7edea52181b56d07be531adc82fea516
Simon Truong <simontruong2@gmail.com>
2020-06-27 15:50:24 +1000

    WIP: added new component for country codes

---
+++ components/country-codes/country-codes-table.vue
@@ -0,0 +1,15 @@
+<template
+  ><div>
+    <h1>testing</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import countryCodes from './country-codes.js';
+
+@Component
+export default class GoogleCharts extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
